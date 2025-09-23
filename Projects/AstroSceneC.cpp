695e37efc470838acbcd5074716c67d667306227
glscene <pavelvvassiliev@gmail.com>
2025-09-23 12:47:40 +0300

    Updated AstroSceneC

--- Projects/AstroSceneC.cpp
+++ Projects/AstroSceneC.cpp
@@ -4,6 +4,13 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
+USEFORM("..\source\astroc\fcForm.cpp", FormC);
+USEFORM("..\source\astroc\fcAstroScene.cpp", frmAstroScene);
+USEFORM("..\source\astroc\dcImages.cpp", dmImages);
+USEFORM("..\source\astroc\dcDialogs.cpp", dmDialogs);
+USEFORM("..\source\astroc\dcBase.cpp", dmBase);
+
+//---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
 	try
@@ -11,9 +18,9 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->Initialize();
 		Application->MainFormOnTaskBar = true;
 		Application->CreateForm(__classid(TfrmAstroScene), &frmAstroScene);
-		Application->CreateForm(__classid(TdmBase), &dmBase);
-		Application->CreateForm(__classid(TdmDialogs), &dmDialogs);
 		Application->CreateForm(__classid(TdmImages), &dmImages);
+		Application->CreateForm(__classid(TdmDialogs), &dmDialogs);
+		Application->CreateForm(__classid(TdmBase), &dmBase);
 		Application->CreateForm(__classid(TFormC), &FormC);
 		Application->Run();
 	}
