7665d0c4d39c08fafec2a622433f80384c1de8bb
glscene <pavelvvassiliev@gmail.com>
2026-02-08 23:16:05 +0300

    Updated tech sources

---
+++ project/Technoneta.dpr
@@ -0,0 +1,45 @@
+(*
+  Tehnosfera - exoplanets with noos and technospheres
+*)
+program Technoneta;
+uses
+  Forms,
+  ftMeshEditor in '..\source\tech\ftMeshEditor.pas' {frmMeshEditor},
+  ftMeshData in '..\source\tech\ftMeshData.pas' {FormMeshData},
+  ftAstroPilot in '..\source\tech\ftAstroPilot.pas' {frmSpacePilot},
+  fmFormFirst in '..\source\fmFormFirst.pas' {frmFirst},
+  ftCyborg in '..\source\tech\ftCyborg.pas' {frmCyborg},
+  ftRobot in '..\source\tech\ftRobot.pas' {frmRobot},
+  Tech.BoneUtils in '..\source\tech\Tech.BoneUtils.pas',
+  Tech.Globals in '..\source\tech\Tech.Globals.pas',
+  Astro.Utils in '..\source\astro\Astro.Utils.pas',
+  dmBase in '..\source\dmBase.pas' {DataModuleBase: TDataModule},
+  dmDialogs in '..\source\dmDialogs.pas' {DataModuleDialogs: TDataModule},
+  dmImages in '..\source\dmImages.pas' {DataModuleImages: TDataModule},
+  fmAbout in '..\source\fmAbout.pas' {FormAbout},
+  Astro.Camera in '..\source\astro\Astro.Camera.pas',
+  Astro.Globals in '..\source\astro\Astro.Globals.pas',
+  Astro.SkyBodies in '..\source\astro\Astro.SkyBodies.pas',
+  ftTechnoneta in '..\source\tech\ftTechnoneta.pas' {frmTechnoneta},
+  ftLocations in '..\source\tech\ftLocations.pas' {FormLocations},
+  Astro.ReadHyg in '..\source\astro\Astro.ReadHyg.pas',
+  ftEmpire in '..\source\tech\ftEmpire.pas' {FormCETI};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+
+  Application.Title := 'Tehneta';
+  Application.CreateForm(TfrmTechnoneta, frmTechnoneta);
+  Application.CreateForm(TDataModuleBase, DataModuleBase);
+  Application.CreateForm(TDataModuleDialogs, DataModuleDialogs);
+  Application.CreateForm(TDataModuleImages, DataModuleImages);
+  Application.CreateForm(TfrmMeshEditor, frmMeshEditor);
+  Application.CreateForm(TfrmCyborg, frmCyborg);
+  Application.CreateForm(TfrmRobot, frmRobot);
+  Application.CreateForm(TfrmSpacePilot, frmSpacePilot);
+  Application.CreateForm(TfrmFirst, frmFirst);
+  Application.CreateForm(TFormCETI, FormCETI);
+  Application.Run;
+end.
--- source/galaxy/fgGalaqtium_ru.dfm
+++ source/galaxy/fgGalaqtium_ru.dfm
@@ -42,6 +42,7 @@ object frmGalaqtium: TfrmGalaqtium
     Height = 33
     Align = alTop
     TabOrder = 1
+    ExplicitWidth = 974
     object ToolBarMain: TToolBar
       Left = 19
       Top = 2
